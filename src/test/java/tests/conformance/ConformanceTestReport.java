eff613a76a174612a3f5075b18f9d485dba051f5
David P. Baker <dpb@google.com>
2023-08-18 15:21:38 -0400

    Formatting.

--- src/test/java/tests/conformance/ConformanceTestReport.java
+++ src/test/java/tests/conformance/ConformanceTestReport.java
@@ -118,8 +118,7 @@ public final class ConformanceTestReport {
           writeFact(report, expectedFact, matchesReportedFact(expectedFact) ? "PASS" : "FAIL");
         }
         if (details) {
-          // Report all unexpected reported facts on this line and whether they must be expected or
-          // not.
+          // Report all unexpected facts on this line and whether they must be expected or not.
           for (ReportedFact reportedFact : reportedFactsInFile.get(lineNumber)) {
             if (isUnexpected(reportedFact)) {
               writeFact(report, reportedFact, reportedFact.mustBeExpected() ? "OOPS" : "INFO");
