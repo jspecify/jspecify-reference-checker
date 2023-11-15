5d21c9a60f57140307cc6f039cfe572a7a0fea0f
David P. Baker <dpb@google.com>
2023-11-15 14:49:03 -0500

    Move conformance test framework code to a new package. (#109)
    Part of #107.

--- conformance/src/main/java/org/jspecify/conformance/AbstractConformanceTest.java
+++ conformance/src/main/java/org/jspecify/conformance/AbstractConformanceTest.java
@@ -1,4 +1,18 @@
-package tests.conformance;
+// Copyright 2023 The JSpecify Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.jspecify.conformance;
 
 import static com.google.common.collect.ImmutableList.toImmutableList;
 import static com.google.common.collect.Lists.partition;
@@ -13,7 +27,7 @@ import static java.util.Arrays.stream;
 import static java.util.Objects.requireNonNull;
 import static java.util.Objects.requireNonNullElse;
 import static java.util.stream.Collectors.toList;
-import static tests.conformance.AbstractConformanceTest.ExpectedFact.readExpectedFact;
+import static org.jspecify.conformance.AbstractConformanceTest.ExpectedFact.readExpectedFact;
 
 import com.google.common.base.Ascii;
 import com.google.common.base.Strings;
