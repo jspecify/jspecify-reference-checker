f48f88f96641244413061bc9b81a0e44dd99262e
Werner Dietl <wdietl@gmail.com>
2024-01-04 11:00:01 -0500

    Use EISOP CF sources (#118)
    Co-authored-by: David P. Baker <dpb@google.com>

--- src/main/java/com/google/jspecify/nullness/NullSpecChecker.java
+++ src/main/java/com/google/jspecify/nullness/NullSpecChecker.java
@@ -23,7 +23,7 @@ import com.sun.source.util.TaskListener;
 import com.sun.source.util.TreePath;
 import com.sun.tools.javac.processing.JavacProcessingEnvironment;
 import com.sun.tools.javac.util.Log;
-import java.util.SortedSet;
+import java.util.NavigableSet;
 import java.util.TreeSet;
 import javax.lang.model.element.TypeElement;
 import org.checkerframework.common.basetype.BaseTypeChecker;
@@ -62,8 +62,8 @@ public final class NullSpecChecker extends BaseTypeChecker {
   public NullSpecChecker() {}
 
   @Override
-  public SortedSet<String> getSuppressWarningsPrefixes() {
-    SortedSet<String> prefixes = new TreeSet<>();
+  public NavigableSet<String> getSuppressWarningsPrefixes() {
+    TreeSet<String> prefixes = new TreeSet<>();
     prefixes.add("nullness");
     return prefixes;
   }
