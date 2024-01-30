d19fed1c9ddc58c7383a9e0b3f4b5ad3b29ffbb4
Werner Dietl <wdietl@gmail.com>
2024-01-29 21:18:09 -0500

    Only handle messages with details

--- src/test/java/tests/ConformanceTest.java
+++ src/test/java/tests/ConformanceTest.java
@@ -147,6 +147,7 @@ public final class ConformanceTest {
     return result.getUnexpectedDiagnostics().stream()
         .map(d -> DetailMessage.parse(d.getMessage(), testDirectory))
         .filter(Objects::nonNull)
+        .filter(DetailMessage::hasDetails)
         .map(DetailMessageReportedFact::new)
         .collect(toImmutableSet());
   }
