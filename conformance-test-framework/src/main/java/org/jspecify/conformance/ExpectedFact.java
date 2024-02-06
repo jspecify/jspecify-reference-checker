775500946843ba147a6cee2dab2c43d24b3ec422
David P. Baker <dpb@google.com>
2024-02-06 10:16:24 -0500

    Support test names by comment (#148)
    Partially fixes #141.

--- conformance-test-framework/src/main/java/org/jspecify/conformance/ExpectedFact.java
+++ conformance-test-framework/src/main/java/org/jspecify/conformance/ExpectedFact.java
@@ -15,10 +15,15 @@
 package org.jspecify.conformance;
 
 import static com.google.common.base.MoreObjects.toStringHelper;
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.collect.ImmutableSetMultimap.toImmutableSetMultimap;
 import static java.util.stream.Collectors.joining;
 
+import com.google.common.base.CharMatcher;
+import com.google.common.base.Joiner;
 import com.google.common.collect.ImmutableList;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.ListIterator;
@@ -26,6 +31,7 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import org.jspecify.annotations.Nullable;
 
 /**
  * An assertion that the tool behaves in a way consistent with a specific fact about a line in the
@@ -47,11 +53,14 @@ public final class ExpectedFact extends Fact {
           Pattern.compile("test:irrelevant-annotation:\\S+"),
           Pattern.compile("test:sink-type:[^:]+:.*"));
 
+  private final @Nullable String testName;
   private final String factText;
   private final long factLineNumber;
 
-  ExpectedFact(Path file, long lineNumber, String factText, long factLineNumber) {
+  ExpectedFact(
+      Path file, long lineNumber, @Nullable String testName, String factText, long factLineNumber) {
     super(file, lineNumber);
+    this.testName = testName;
     this.factText = factText;
     this.factLineNumber = factLineNumber;
   }
@@ -74,6 +83,11 @@ public final class ExpectedFact extends Fact {
   }
 
   @Override
+  public String getIdentifier() {
+    return testName == null ? super.getIdentifier() : testName;
+  }
+
+  @Override
   public boolean equals(Object o) {
     if (this == o) {
       return true;
@@ -84,19 +98,21 @@ public final class ExpectedFact extends Fact {
     ExpectedFact that = (ExpectedFact) o;
     return this.getFile().equals(that.getFile())
         && this.getLineNumber() == that.getLineNumber()
+        && Objects.equals(this.testName, that.testName)
         && this.factText.equals(that.factText)
         && this.factLineNumber == that.factLineNumber;
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(getFile(), getLineNumber(), factText);
+    return Objects.hash(getFile(), getLineNumber(), testName, factText);
   }
 
   @Override
   public String toString() {
     return toStringHelper(this)
         .add("file", getFile())
+        .add("testName", testName)
         .add("lineNumber", getLineNumber())
         .add("factText", factText)
         .add("factLineNumber", factLineNumber)
@@ -108,35 +124,96 @@ public final class ExpectedFact extends Fact {
 
     private static final Pattern EXPECTATION_COMMENT =
         Pattern.compile(
-            "\\s*// "
+            "\\s*// ("
+                + "(test:name:(?<testName>.*))"
+                + "|"
                 + ("(?<fact>"
                     + FACT_PATTERNS.stream().map(Pattern::pattern).collect(joining("|"))
                     + ")")
-                + "\\s*");
+                + ")\\s*");
+
+    private static final CharMatcher ASCII_DIGIT = CharMatcher.inRange('0', '9');
+
+    private final Map<Long, String> facts = new HashMap<>();
+    private final List<String> errors = new ArrayList<>();
+
+    private Path file;
+    private @Nullable String testName;
+    private long lineNumber;
 
     /** Reads expected facts from lines in a file. */
     ImmutableList<ExpectedFact> readExpectedFacts(Path file, List<String> lines) {
+      this.file = file;
       ImmutableList.Builder<ExpectedFact> expectedFacts = ImmutableList.builder();
-      Map<Long, String> facts = new HashMap<>();
       ListIterator<String> i = lines.listIterator();
       while (i.hasNext()) {
         String line = i.next();
-        long lineNumber = i.nextIndex();
+        lineNumber = i.nextIndex();
         Matcher matcher = EXPECTATION_COMMENT.matcher(line);
         if (matcher.matches()) {
+          setTestName(matcher.group("testName"));
           String fact = matcher.group("fact");
           if (fact != null) {
             facts.put(lineNumber, fact.trim());
           }
         } else {
+          if (testName != null) {
+            check(!facts.isEmpty(), "no expected facts for test named %s", testName);
+          }
           facts.forEach(
               (factLineNumber, factText) ->
-                  expectedFacts.add(new ExpectedFact(file, lineNumber, factText, factLineNumber)));
+                  expectedFacts.add(
+                      new ExpectedFact(file, lineNumber, testName, factText, factLineNumber)));
           facts.clear();
+          testName = null;
         }
       }
-      // TODO(netdpb): Report an error if facts is not empty.
-      return expectedFacts.build();
+      return checkUniqueTestNames(expectedFacts.build());
+    }
+
+    private void setTestName(@Nullable String testName) {
+      if (testName == null) {
+        return;
+      }
+      check(this.testName == null, "test name already set");
+      check(facts.isEmpty(), "test name must come before assertions for a line");
+      this.testName = checkTestName(testName.trim());
+    }
+
+    private boolean check(boolean test, String format, Object... args) {
+      if (!test) {
+        errors.add(String.format("  %s:%d: %s", file, lineNumber, String.format(format, args)));
+      }
+      return test;
+    }
+
+    private String checkTestName(String testName) {
+      if (check(!testName.isEmpty(), "test name cannot be empty")) {
+        check(!testName.contains(":"), "test name cannot contain a colon: %s", testName);
+        check(!ASCII_DIGIT.matchesAllOf(testName), "test name cannot be an integer: %s", testName);
+      }
+      return testName;
+    }
+
+    private ImmutableList<ExpectedFact> checkUniqueTestNames(
+        ImmutableList<ExpectedFact> expectedFacts) {
+      expectedFacts.stream()
+          .filter(ef -> ef.testName != null)
+          .collect(toImmutableSetMultimap(ef -> ef.testName, ExpectedFact::getLineNumber))
+          .asMap()
+          .forEach(
+              (testName, lineNumbers) ->
+                  check(
+                      lineNumbers.size() == 1,
+                      "test name not unique: test '%s' appears on tests of lines %s",
+                      testName,
+                      lineNumbers));
+      return expectedFacts;
+    }
+
+    /** Throws if there were any errors encountered while reading expected facts. */
+    void checkErrors() {
+      checkArgument(errors.isEmpty(), "errors in test inputs:\n%s", Joiner.on('\n').join(errors));
     }
   }
 }
